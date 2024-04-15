0b242863b16de86c89e1256371ddd7c54c8af7e8
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2024-04-15 18:27:48 +0530

    Events List

---
+++ Sports Management System/Sports Management System/Controllers/EventController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sports_Management_System.Models.ViewModels;
+using Sports_Management_System.Repository.IRepository;
+
+namespace Sports_Management_System.Controllers
+{
+    [Authorize]
+    public class EventController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public EventController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var events = _unitOfWork.Event.GetAll().OrderBy(x => x.FeatureEvent).ToList();
+            var games = _unitOfWork.Game.GetAll().ToList();
+
+            var returnModel = new List<EventBaseVm>();
+
+            if (events != null && events.Count > 0)
+            {
+                foreach (var @event in events)
+                {
+                    var vm = new EventBaseVm()
+                    {
+                        Id = @event.Id,
+                        Game = games?.Where(x => x.Id == @event.GameId)?.FirstOrDefault()?.Name,
+                        FeatureEvent = @event.FeatureEvent,
+                        EventVenue = @event.EventVenue,
+                        EventDate = @event.EventDate,
+                        EventStartTime = @event.EventStartTime,
+                        EventEndTime = @event.EventEndTime,
+                    };
+
+                    returnModel.Add(vm);
+                }
+            }
+
+            return View(returnModel);
+        }
+    }
+}
---
+++ Sports Management System/Sports Management System/Models/ViewModels/EventVm.cs
@@ -0,0 +1,13 @@
+﻿namespace Sports_Management_System.Models.ViewModels
+{
+    public class EventBaseVm
+    {
+        public Guid Id { get; set; }
+        public string Game { get; set; }
+        public string FeatureEvent { get; set; }
+        public string EventVenue { get; set; }
+        public DateOnly EventDate { get; set; }
+        public TimeOnly EventStartTime { get; set; }
+        public TimeOnly EventEndTime { get; set; }
+    }
+}
